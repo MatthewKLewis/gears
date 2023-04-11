43f9b93a2aa6b9b959aa5c5c299043337ab6ba1f
MatthewKLewis <matthew.knight.lewis@gmail.com>
2023-04-11 16:54:38 -0400

    colorful

--- src/app/services/gears.service.ts
+++ src/app/services/gears.service.ts
@@ -9,8 +9,37 @@ export class Gear {
   constructor(teeth: number, dir: string) {
     this.teeth = teeth;
     this.toothIndex = 0;
-    this.color = 'lightblue';
     this.dir = dir;
+
+    switch (Math.floor(Math.random() * 9)) {
+      case 1:
+        this.color = 'red'
+        break;
+      case 2:
+        this.color = 'blue'
+        break;
+      case 3:
+        this.color = 'green'
+        break;
+      case 4:
+        this.color = 'purple'
+        break;        
+      case 5:
+        this.color = 'black'
+        break;
+      case 6:
+        this.color = 'yellow'
+        break;
+      case 7:
+        this.color = 'indigo'
+        break;
+      case 8:
+        this.color = 'purple'
+        break;        
+      default:
+        this.color = 'orange'
+        break;
+    }
   }
 }
 
